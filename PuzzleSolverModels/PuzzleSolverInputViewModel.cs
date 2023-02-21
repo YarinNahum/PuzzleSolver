18cc4bf4ef1176170cbc189850fee1ac9e9053f9
Yarin Nahum <yarinnahum4@gmail.com>
2023-02-21 22:53:34 +0200

    added the base view model a puzzle

---
+++ PuzzleSolverModels/PuzzleSolverInputViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PuzzleSolverViewModels
+{
+    public class PuzzleSolverInputViewModel
+    {
+        [Required]
+        public PuzzleType PuzzleType { get; set; }
+        [Required]
+        public PuzzleSolverAlgorithm PuzzleSolverAlgorithm { get; set; }
+        [Required]
+        public int[][]? InitialBoardState { get; set; }
+
+    }
+}
