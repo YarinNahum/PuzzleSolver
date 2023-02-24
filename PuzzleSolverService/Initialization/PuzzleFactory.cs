307eced3a55231edcc38a8658e72dc53f77c3fb2
Yarin Nahum <yarinnahum4@gmail.com>
2023-02-24 13:27:04 +0200

    added factories for puzzles and puzzle states

---
+++ PuzzleSolverService/Initialization/PuzzleFactory.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PuzzleSolverService.Puzzles;
+using PuzzleSolverService.PuzzleStates;
+using PuzzleSolverViewModels;
+
+namespace PuzzleSolverService.Initialization
+{
+    public class PuzzleFactory<T> where T : IEquatable<T>
+    {
+        public static Puzzle<T> CreatePuzzle(PuzzleType type, BoardState<T> initialBoardState)
+        {
+            return type switch
+            {
+                PuzzleType.Sliding => new SlidingPuzzle(initialBoardState as BoardState<int>) as Puzzle<T>,
+                _ => throw new ArgumentException($"Unknown puzzle type: {type}")
+            };
+        }
+    }
+}
---
+++ PuzzleSolverService/Initialization/PuzzleStatesFactory.cs
@@ -0,0 +1,21 @@
+﻿using PuzzleSolverService.PuzzleStates;
+using PuzzleSolverViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PuzzleSolverService.Initialization
+{
+    public static class PuzzleStateFactory<T> where T :IEquatable<T>
+    {
+        public static BoardState<T> CreatePuzzleState(PuzzleType type, T[][] initialState)
+        {
+            return type switch {
+                PuzzleType.Sliding => new SlidingBoardState(initialState as int[][]) as BoardState<T>,
+                _ => throw new ArgumentException($"Unknown puzzle type: {type}")
+            };
+        }
+    }
+}
