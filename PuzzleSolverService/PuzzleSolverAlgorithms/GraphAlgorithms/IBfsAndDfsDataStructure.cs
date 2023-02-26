4172edcdca1246ec9ae4cab4919678885b99fe12
SapirAsoulin <sapirasoulin@gmail.com>
2023-02-26 18:06:58 +0200

    added DFS implementation to solve the puzzles.

---
+++ PuzzleSolverService/PuzzleSolverAlgorithms/GraphAlgorithms/IBfsAndDfsDataStructure.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PuzzleSolverService.PuzzleSolverAlgorithms.GraphAlgorithms
+{
+    public interface IBfsAndDfsDataStructure<T>
+    {
+        /// <summary>
+        /// Add value to the data structure
+        /// </summary>
+        /// <param name="value">The value to add</param>
+        public void AddValue(T value);
+
+        /// <summary>
+        /// Remove a value from the data structure
+        /// </summary>
+        /// <returns>The value that was removed.</returns>
+        public T RemoveValue();
+
+        /// <summary>
+        /// Return the number of values in the data structure.
+        /// </summary>
+        /// <returns></returns>
+        public int Count();
+
+        /// <summary>
+        /// Clear the data structure.
+        /// </summary>
+        public void ClearDataStructure();
+    }
+}
